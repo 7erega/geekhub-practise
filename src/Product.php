4c6de8a48e2ffef3b55075f29c9ab8cdb6d762a8
Serhii Melnyk <serhii.o.melnyk@gmail.com>
2019-10-16 18:32:31 +0300

    adding product class

---
+++ src/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+use Src\Interfaces\Product as iProduct;
+
+class Product implements iProduct {
+  public function add() {
+
+  }
+
+  public function show($productId) {
+
+  }
+
+  public function showAll() {
+
+  }
+
+  public function changeCategory($prodtcId, $categoryId) {
+
+  }
+}
