fb850efcdd31c3517011a7e90a0b1e45303881b7
Serhii Melnyk <serhii.o.melnyk@gmail.com>
2019-10-17 15:01:59 +0300

    creating Category model

---
+++ src/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Src\Models;
+
+class Category {
+
+  static function getCategories() {
+    $file = file_get_contents(__DIR__ . '/../../data/database.json');
+    $array = json_decode($file, TRUE);
+    unset($file);
+
+    $categoriesList = array();
+
+    foreach ($array['categories'] as $category) {
+      $categoryId = array_shift($category);
+      $categoriesList[$categoryId] = $category['name'];
+    }
+
+    return $categoriesList;
+  }
+
+  static function getCategoryName($id) {
+
+  }
+}
